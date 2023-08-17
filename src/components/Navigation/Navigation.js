f363a35faa847387f10c119d82a5beae5fd14f8c
LD\kryukovyar <kryukovyar@yandex-team.ru>
2023-08-17 14:58:12 +0300

    hamburger

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import iconMain from "../../images/icon__COLOR_icon-main.svg";
 import './Navigation.css';
 
 function Navigation() {
+  const [isSidebarActive, setSidebarActive] = useState(false);
+
+  const sidebarClassName = `sidebar ${isSidebarActive ? "sidebar_active" : ""}`;
+
+  function handleButtonBurgerClick(card) {
+    setSidebarActive(true);
+  }
+
+  function handleButtonBurgerCloseClick(card) {
+    setSidebarActive(false);
+  }
+
   return (
     <>
       <div className="header-movies">
@@ -19,9 +32,9 @@ function Navigation() {
           <div className="nav-tab__icon"></div>
         </a>
       </div>
-      <button type="button" className="heder-burger"></button>
-      <div className="sidebar sidebar_active">
-        <button type="button" className="sidebar__icon"></button>
+      <button type="button" className="heder-burger" onClick={handleButtonBurgerClick}></button>
+      <div className={sidebarClassName}>
+        <button type="button" className="sidebar__icon" onClick={handleButtonBurgerCloseClick}></button>
         <div className="nav-burger nav-burger_active">
           <Link to="/" className="nav-burger__list">
               Главная
